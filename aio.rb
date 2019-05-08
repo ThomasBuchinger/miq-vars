19bcf81cc85127504e5e9a5e47d8ddb3209f4ff8
Thomas Buchinger <thomas.buchinger@outlook.com>
2019-05-08 16:32:09 +0200

    add list command and resolve user

--- aio.rb
+++ aio.rb
@@ -10,13 +10,14 @@ at_exit do
   if $ERROR_INFO
     # Find the correct ExitCode
     # and do not print the stack trace
-    $ERROR_INFO.set_backtrace(nil) unless !$settings.nil? && $settings.fetch(:log_level, nil) == :debug
+    #$ERROR_INFO.set_backtrace(nil) unless !$settings.nil? && $settings.fetch(:log_level, nil) == :debug
     exit 1
   end
 end
 
-# frozen_string_literal: true
-
+# =============================================================
+# = CLI configuration
+# =============================================================
 require 'thor'
 module MiqVar
   module Cli
@@ -26,15 +27,26 @@ module MiqVar
       end
 
       desc "show TASK_ID", "Show Information about a given Task (e.g. r1234_miq_provision_1234)"
+      option :format, type: :string, desc: "Values: all, state, short"
       def show(task_id)
         task = MiqVar.task_by_label(task_id)
-        puts task.get_details()
+        format = options.fetch(:format, 'short')
+        raise "Invalid format #{format}" unless %q[all short state].include?(format)
+        puts task.get_details(format)
+      end
+
+      desc "list", "List currently active requests"
+      def list
+        puts MiqQueue.where(class_name: 'MiqAeEngine').map{|q| q.tracking_label }.compact
       end
     end
   end
 end
-# frozen_string_literal: true
 
+
+# =============================================================
+# = Global Methods
+# =============================================================
 module MiqVar
   # Check if we run in a ManageIQ rails context
   def self.validate()
@@ -51,6 +63,9 @@ module MiqVar
 end
 # frozen_string_literal: true
 
+# =============================================================
+# = Task Handling
+# =============================================================
 module MiqVar
   class Task
     attr_reader :data, :tracking_label
@@ -63,13 +78,15 @@ module MiqVar
       @data = miq_queue.args.first.presence || {}
     end
 
-    def get_details()
+    def get_details(format)
       lines = []
+      user = MiqAeMethodService::MiqAeServiceUser.where(id: @data[:user_id]).first
+
       lines << "Task: #{@tracking_label}"
-      lines << "  State: #{@data[:state]}: #{@data[:ae_state_retries]} Retriesi. Last at: #{@data[:ae_state_started]} (I have no idea what that timestamp actually is)"
-      lines << "  User: #{@data[:user_id]}/#{@data[:miq_group_id]}"
-      lines << "-"*50
-      lines << YAML.load(@data[:ae_state_data]).to_yaml
+      lines << "  State: #{@data[:state]}: #{@data[:ae_state_retries]} Retries. Last at: #{nil} "
+      lines << "  User: #{user.userid} (#{user.name})/#{user.current_group.description}"
+      lines << "-"*50 
+      lines << YAML.load(@data[:ae_state_data].presence || "").to_yaml
       lines << "="*50
       lines
     end
