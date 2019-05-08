1e12295522a42f4d4053fb4255c440554c72408f
Thomas Buchinger <thomas.buchinger@outlook.com>
2019-05-08 16:32:55 +0200

    add set command

--- aio.rb
+++ aio.rb
@@ -39,6 +39,12 @@ module MiqVar
       def list
         puts MiqQueue.where(class_name: 'MiqAeEngine').map{|q| q.tracking_label }.compact
       end
+
+      desc "set TASK_ID KEY JSON_VALUE", "Set (Overwrite) a state variable"
+      def set(task_id, key, value)
+        task = MiqVar.task_by_label(task_id)
+        task.set_state_var(key, YAML.load(value))
+      end
     end
   end
 end
@@ -68,25 +74,61 @@ end
 # =============================================================
 module MiqVar
   class Task
-    attr_reader :data, :tracking_label
+    attr_reader :miq_queue_object, :data, :tracking_label
 
     def initialize(tracking_label)
       @tracking_label = tracking_label
-      miq_queue = MiqQueue.where(tracking_label: tracking_label).first
-      raise "Unable to find Task: #{@tracking_label}" if miq_queue.blank?
+      @miq_queue_object = MiqAeMethodService::MiqAeServiceMiqQueue.where(tracking_label: tracking_label).first
+      raise "Unable to find Task: #{@tracking_label}" if @miq_queue_object.blank?
       
-      @data = miq_queue.args.first.presence || {}
+      reload()
+    end
+
+    def reload()
+      @miq_queue_object.reload()
+      raise "Task vanished from Queue. (is it finished?)" if @miq_queue_object.blank?
+
+      args = @miq_queue_object.args
+      if !args.kind_of? Array || args.try(:compact).try(:length) != 1
+        puts args.pretty_inspect
+        raise "Arg does not look right! (quiting for safety reasons)"
+      end
+      @data = args.first
+    end
+
+    def set_state_var(key, value)
+      reload()
+
+      puts "Setting State Var: #{key} to #{value}"
+      vars = YAML.load(@data[:ae_state_data])
+      vars[key] = value
+
+      new_data = @data.dup
+      new_data[:ae_state_data] = vars.to_yaml
+      @miq_queue_object.attributes = {'args': [ new_data ]}
+#      puts @miq_queue_object.inspect
+      @miq_queue_object.instance_variable_get(:@object).save
+      reload()
+      puts "="*50
+      puts @data.to_yaml
     end
 
     def get_details(format)
       lines = []
       user = MiqAeMethodService::MiqAeServiceUser.where(id: @data[:user_id]).first
+      state_data = YAML.load(@data[:ae_state_data].presence || "")
 
       lines << "Task: #{@tracking_label}"
-      lines << "  State: #{@data[:state]}: #{@data[:ae_state_retries]} Retries. Last at: #{nil} "
+      lines << "  Root: object_type=#{@data[:object_type]} Id=#{@data[:object_id]}"
+      lines << "  Automate: URI=#{@data[:namespace]}/#{@data[:class_name]}/#{@data[:instance_name]} Step=#{@data[:state]}"
+      lines << "  State: #{@data[:ae_state_retries]} Retries. Next: #{@miq_queue_object.deliver_on}"
       lines << "  User: #{user.userid} (#{user.name})/#{user.current_group.description}"
-      lines << "-"*50 
-      lines << YAML.load(@data[:ae_state_data].presence || "").to_yaml
+      lines << "-"*50
+      if format == "short"
+        lines << state_data.keys
+      else 
+        lines << state_data.to_yaml
+      end
       lines << "="*50
       lines
     end
