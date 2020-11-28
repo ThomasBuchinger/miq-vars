d319c0640fb4385ac0b05ba10d2f74c3974a8cc6
root <root@cfmep03.eb.lan.at>
2020-11-28 21:31:07 +0100

    Allow modification of ResourceActions

--- aio.rb
+++ aio.rb
@@ -25,19 +25,25 @@ module MiqVar
       def self.exit_on_failure?
         true
       end
+      class_option :index, type: :numeric, default: 0, desc: "Select the correct StateMachine, if there are multiple with the same TASK_ID"
 
-      desc "show TASK_ID", "Show Information about a given Task (e.g. r1234_miq_provision_1234)"
-      option :format, type: :string, desc: "Values: all, state, short"
-      def show(task_id)
-        task = MiqVar.task_by_label(task_id)
+      desc "show TASK_ID [all][STATE_VAR, ...]", "Show Information about a given Task (e.g. r1234_miq_provision_1234)"
+      def show(task_id, *state_var)
         format = options.fetch(:format, 'short')
-        raise "Invalid format #{format}" unless %q[all short state].include?(format)
-        puts task.get_details(format)
+        task   = MiqVar.task_by_label(task_id, options[:index])
+
+        puts task.get_details(state_var)
       end
 
       desc "list", "List currently active requests"
       def list
         puts MiqQueue.where(class_name: 'MiqAeEngine').map{|q| q.tracking_label }.compact
+      end
+
+      desc "set TASK_ID KEY JSON_VALUE", "Set (Overwrite) a state variable"
+      def set(task_id, key, value)
+        task = MiqVar.task_by_label(task_id, options[:index])
+        task.set_state_var(key, YAML.load(value))
       end
     end
   end
@@ -54,10 +60,10 @@ module MiqVar
 
   end
 
-  def self.task_by_label(tracking_label)
-    raise "Malformatted task_id #{tracking_label}" unless tracking_label =~ /^r[\d]+/
+  def self.task_by_label(tracking_label, index=0)
+    raise "Malformatted task_id #{tracking_label}" unless tracking_label =~ /^r[\d]+/ || tracking_label.start_with?('resource_action')
 
-    MiqVar::Task.new(tracking_label)
+    MiqVar::Task.new(tracking_label, index)
   end
 
 end
@@ -68,25 +74,63 @@ end
 # =============================================================
 module MiqVar
   class Task
-    attr_reader :data, :tracking_label
+    attr_reader :miq_queue_object, :data, :tracking_label
 
-    def initialize(tracking_label)
+    def initialize(tracking_label, index=0)
       @tracking_label = tracking_label
-      miq_queue = MiqQueue.where(tracking_label: tracking_label).first
-      raise "Unable to find Task: #{@tracking_label}" if miq_queue.blank?
+      @miq_queue_object = MiqAeMethodService::MiqAeServiceMiqQueue.where(tracking_label: tracking_label).try(:[], index)
+      raise "Unable to find Task: #{@tracking_label}" if @miq_queue_object.blank?
       
-      @data = miq_queue.args.first.presence || {}
+      reload()
     end
 
-    def get_details(format)
+    def reload()
+      @miq_queue_object.reload()
+      raise "Task vanished from Queue. (is it finished?)" if @miq_queue_object.blank?
+
+      args = @miq_queue_object.args
+      if !args.kind_of? Array || args.try(:compact).try(:length) != 1
+        puts args.pretty_inspect
+        raise "Arg does not look right! (quiting for safety reasons)"
+      end
+      @data = args.first
+    end
+
+    def set_state_var(key, value)
+      reload()
+
+      puts "Setting State Var: #{key} to #{value}"
+      vars = YAML.load(@data[:ae_state_data])
+      vars[key] = value
+
+      new_data = @data.dup
+      new_data[:ae_state_data] = vars.to_yaml
+      @miq_queue_object.attributes = {'args': [ new_data ]}
+#      puts @miq_queue_object.inspect
+      @miq_queue_object.instance_variable_get(:@object).save
+      reload()
+      puts "="*50
+      puts @data.to_yaml
+    end
+
+    def get_details(selected_vars)
       lines = []
       user = MiqAeMethodService::MiqAeServiceUser.where(id: @data[:user_id]).first
+      state_data = YAML.load(@data[:ae_state_data].presence || "")
 
       lines << "Task: #{@tracking_label}"
-      lines << "  State: #{@data[:state]}: #{@data[:ae_state_retries]} Retries. Last at: #{nil} "
+      lines << "  Root: object_type=#{@data[:object_type]} Id=#{@data[:object_id]}"
+      lines << "  Automate: URI=#{@data[:namespace]}/#{@data[:class_name]}/#{@data[:instance_name]} Step=#{@data[:state]}"
+      lines << "  State: #{@data[:ae_state_retries]} Retries. Next: #{@miq_queue_object.deliver_on}"
       lines << "  User: #{user.userid} (#{user.name})/#{user.current_group.description}"
-      lines << "-"*50 
-      lines << YAML.load(@data[:ae_state_data].presence || "").to_yaml
+      lines << "-"*50
+      if selected_vars.blank?
+        lines << state_data.keys
+      elsif selected_vars.include?('all')
+        lines << state_data.to_yaml
+      else
+        lines << state_data.select{|k,_| selected_vars.include?(k) }.to_yaml
+      end
       lines << "="*50
       lines
     end
